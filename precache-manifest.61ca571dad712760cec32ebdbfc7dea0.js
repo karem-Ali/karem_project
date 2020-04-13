840cfd4722c7ff1fe7d525201d203f0d5ee1db1c
karem-Ali <karem.phones2018@gmail.com>
2020-04-13 23:00:55 +0200

    Updates

---
+++ precache-manifest.61ca571dad712760cec32ebdbfc7dea0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44442da186a63be414eafd14d49f463",
+    "url": "/karem_project/index.html"
+  },
+  {
+    "revision": "8cf97992ab0d8a7430e1",
+    "url": "/karem_project/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "197f99b6acaaf4979107",
+    "url": "/karem_project/static/css/main.7af7057d.chunk.css"
+  },
+  {
+    "revision": "8cf97992ab0d8a7430e1",
+    "url": "/karem_project/static/js/2.84d6e4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karem_project/static/js/2.84d6e4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "197f99b6acaaf4979107",
+    "url": "/karem_project/static/js/main.cb2f02b3.chunk.js"
+  },
+  {
+    "revision": "993f162f91ce2c9ca2f3",
+    "url": "/karem_project/static/js/runtime-main.6faa7f64.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/karem_project/static/media/SEGA.12a3db8b.woff"
+  }
+]);
